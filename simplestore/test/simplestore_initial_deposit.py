61c5bcc7582bfa8021e3336f72a92ec5266217ed
Huw Morris <huw.morris@stfc.ac.uk>
2013-08-21 13:53:57 +0100

    Read JSON file and deposit records

--- simplestore/test/simplestore_initial_deposit.py
+++ simplestore/test/simplestore_initial_deposit.py
@@ -1,22 +1,30 @@
-import sys 
+import sys
+import json
+import requests
 
 from invenio.testutils import (make_test_suite, run_test_suite, InvenioTestCase,
                                test_web_page_content,
                                get_authenticated_mechanize_browser)
 from invenio.config import CFG_SITE_SECURE_URL
 from bs4 import BeautifulSoup
-import requests
 
 
+class DepositException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)    
 
+    
+def read_json(json_file):
+    with open(json_file) as js:
+        data = json.load(js)
+    return data
 
-        
-def main():
-        """Test "happy path" - deposit a new file with metadata"""
-
-        proxy = {'http' : 'wwwcache.rl.ac.uk:8080',
-                 'https' : 'wwwcache.rl.ac.uk:8080'}
-
+            
+def deposit_item(record, verbose, rec_num):
+        if verbose:
+            print "Record " + str(rec_num) + ": ...", 
         br = get_authenticated_mechanize_browser(username="admin", password="")
         res = br.open(CFG_SITE_SECURE_URL + '/deposit')
         dep_soup = BeautifulSoup(res.get_data())
@@ -28,21 +36,18 @@ def main():
         login = requests.post('%s/youraccount/login' % CFG_SITE_SECURE_URL,
                               data=form,
                               verify=False)
-                                                 
-        form = {'name': "simplestore_test_text.txt"}
-        with open('data/simplestore_test_text.txt') as f:
-            files = {'file': f}
-            r = requests.post("%s/deposit/upload/%s" % (CFG_SITE_SECURE_URL, sub_id),
-                          files=files,
-                          data=form,
-                          verify=False,
-                          cookies=login.cookies)
         
-        form = {'name': "simplestore_test_image.jpeg"}
-        with open('data/simplestore_test_image.jpeg') as f:
-            print "Opened file"
-            files = {'file': f.read()}
-            r = requests.post("%s/deposit/upload/%s" % (CFG_SITE_SECURE_URL, sub_id),
+        files_list = record["files"]
+        for fl in files_list:
+            if fl.has_key('dir'):
+                test_data_dir = fl["dir"]
+            else:
+                test_data_dir = 'data/'
+            filename = fl["filename"]                             
+            form = {'name': filename}
+            with open('%s%s' % (test_data_dir, filename)) as f:
+                files = {'file': f.read()}
+                r = requests.post("%s/deposit/upload/%s" % (CFG_SITE_SECURE_URL, sub_id),
                           files=files,
                           data=form,
                           verify=False,
@@ -53,22 +58,60 @@ def main():
         br['domain'] = ['Generic']
         resp = br.submit()
 
-        #print resp.read()
-
         #add some metadata
         br.select_form(nr=0)
-        br['title'] = 'Test Title'
-        br['description'] = 'Test Description'
-        br['creator'] = 'Test Bot'
-        
+        metadata_dict = record["metadata"]
+        key_list = metadata_dict.keys()
+        title = "Untitled"
+        for key in key_list:
+            br[key] = metadata_dict[key]
+            if (key == 'title'):
+                title = metadata_dict[key]
+              
         resp = br.submit()
         resp_text = resp.read()
-        #print resp_text
+
         #check we got to the last page
-        #self.assertIn("Your submission will shortly be available", resp_text)
+        if "Your submission will shortly be available" in resp_text:
+            if verbose:
+                print "Successful"
+        else:    
+            raise DepositException("Unable to deposit record: " + title +
+                '\n' + str(record))
 
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+    if len(argv) > 3:
+        print "Usage: " + argv[0] + " [-v] [json file]"
+        return
+           
+    # set defaults
+    json_file = "test_data.json"
+    verbose = False
 
-#TEST_SUITE = make_test_suite(DepositTest)
+    if len(argv) > 1:
+        if argv[1] == '-v' or argv[1] == '--verbose':
+            verbose = True
+            del argv[1]
+        
+        if len(argv) > 1:    
+            json_file = argv[1]
+        
+    test_data = read_json(json_file)
 
+    records_list = test_data["records"]
+    if verbose:
+        print "Depositing " + str(len(records_list)) + " records"
+    rec_num = 0    
+    for r in records_list:
+        rec_num = rec_num + 1
+        try:
+            deposit_item(r["record_content"], verbose, rec_num)
+        except DepositException as e:
+            print e.value
+
+        
 if __name__ == '__main__':
     main()
+    
