7ffcdc08a3dbdeb4a3fee63ba8d9bacca337d38f
Klara Peskova <ustav@seznam.cz>
2014-04-02 22:58:40 +0200

    Choices to linguistics

--- simplestore/lib/simplestore_model/HTML5ModelConverter.py
+++ simplestore/lib/simplestore_model/HTML5ModelConverter.py
@@ -208,8 +208,8 @@ class AddFieldInput(Input):
     def __call__(self, field, **kwargs):
         kwargs.setdefault('id', field.id)
         html = ['<div id="itemRows">']
-        html.append('<p><input placeholder="{0}" {1}>'.format(
-             field.placeholder, self.html_params(name=field.name, **kwargs)))                             
+        html.append('<p id="rowNum0"><input placeholder="{0}" {1}>'.format(
+             field.placeholder, self.html_params(name=field.name+"[]", **kwargs)))                             
         html.append('<input onclick="addRow(this.form, \'{0}\', {1});" type="button" value="Add row" ></p>'
             .format(field.placeholder, field.name))              
         html.append('</div>')
